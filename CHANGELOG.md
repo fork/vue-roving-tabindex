099e7958a35cd1fe533db350f438f44c70f9942c
Leonard Techel <ltechel@fork.de>
2019-12-13 18:05:25 +0100

    Use rollup to create a transpiled bundle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Use rollup to create a transpiled bundle
+
 ## 1.1.0 - 2019-11-05
 
 ### Added
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from "rollup-plugin-babel";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: pkg.main,
+    format: "es"
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**" // only transpile our source code
+    })
+  ],
+  external: ["vue"]
+};
